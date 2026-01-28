aa80e76ac3fda150fa58bdd0966a1d42b4ec7c34
slap.red Lead Dev <slapredgit@gmail.com>
2026-01-29 04:49:20 +1000

    feat(setup): Add platform detection

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-pkg update && pkg upgrade -y
+
+if [ -d "/data/data/com.termux" ]; then
+  PLATFORM="Termux"
+elif [ -n "$PSVersionTable" ]; then
+  PLATFORM="PowerShell"
+elif grep -q "Microsoft" /proc/version; then
+  PLATFORM="WSL"
+else
+  PLATFORM="Unknown"
+fi
+
+echo "Detected platform: $PLATFORM"
